245c7880382a309dad456d424ca4f1a74cfd93df
D-Sto <55364206+D-Sto@users.noreply.github.com>
2019-11-20 04:28:06 -0500

    Update HomeActivity.java

--- app/src/main/java/com/example/project/HomeActivity.java
+++ app/src/main/java/com/example/project/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.example.project;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,12 +12,21 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
     Button btnLogout;
+    Button mFilter;
+    TextView mOptionFiltered;
+    String[] listItems;
+    boolean[] checkedItems;
+    ArrayList<Integer> mUserItems = new ArrayList<>();
+
     private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
@@ -39,6 +51,69 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mFilter = (Button) findViewById(R.id.btnFilter);
+        mOptionFiltered = (TextView) findViewById(R.id.tvOptionFiltered);
+
+        listItems = getResources().getStringArray(R.array.Game_Item); //holds different sports
+        checkedItems = new boolean[listItems.length]; //will act as checkboxes
+
+        mFilter.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(HomeActivity.this);
+                mBuilder.setTitle(getString(R.string.dialog_title));
+                mBuilder.setMultiChoiceItems(listItems, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int position, boolean isChecked) {
+                        if(isChecked){
+                            if(!mUserItems.contains(position)){
+                                mUserItems.add(position);
+                            }
+                        } else if(mUserItems.contains(position))
+                        {
+                            mUserItems.remove((Integer)position); //cast to int otherwise app will crash
+                        }
+                    }
+                });
+                mBuilder.setCancelable(false);
+                mBuilder.setPositiveButton(R.string.ok_label, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        String item = "";
+                        for (int i = 0; i < mUserItems.size(); i++){
+                            item = item + listItems[mUserItems.get(i)]; // passing in i, the position
+                            if(i != mUserItems.size() - 1) {
+                                item = item + ", ";
+                            }
+                        }
+                        mOptionFiltered.setText(item);
+                    }
+                });
+
+                mBuilder.setNegativeButton(R.string.dismiss_label, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+
+                mBuilder.setNeutralButton(R.string.clear_all_label, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        for (int i = 0; i < checkedItems.length; i++){
+                            checkedItems[i] = false;
+                            mUserItems.clear();
+                            mOptionFiltered.setText(""); //setting empty text for the field
+                        }
+                    }
+                });
+
+                AlertDialog mDialog = mBuilder.create();
+                mDialog.show();
+            }
+        });
 
 
     }
