59b43737b642e31f3d4e0ddc98473e5d51abca71
D-Sto <55364206+D-Sto@users.noreply.github.com>
2019-12-05 22:28:00 -0500

    Add files via upload

---
+++ app/src/androidTest/java/com/example/project/GameTest.java
@@ -0,0 +1,33 @@
+package com.First.ISportsC.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    @Test
+    public void setTitle() {
+    }
+
+    @Test
+    public void getChatroom_id() {
+    }
+
+    @Test
+    public void setChatroom_id() {
+    }
+
+    @Test
+    public void toString1() {
+    }
+
+    @Test
+    public void describeContents() {
+        assertEquals(0,0);
+    }
+
+    @Test
+    public void writeToParcel() {
+    }
+}
